f1f3957b15c30269acd12e2091e8d2d8b5ea92ed
Sergio Arroutbi <sarroutb@redhat.com>
2022-03-18 11:51:50 +0100

    Fix output messages after tang-operator configuration
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tang-operator/start.sh
+++ tang-operator/start.sh
@@ -2,7 +2,8 @@
 sleep 5
 launch-k8s.sh
 install-operator-sdk.sh
-echo "==================================================================="
-echo "          K8S cluster is ready, operator-sdk is installed          "
-echo "                  Please, continue with next steps                 "
-echo "==================================================================="
+echo "
+===================================================================
+          K8S cluster is ready, operator-sdk is installed          
+                  Please, continue with next steps                 
+==================================================================="
