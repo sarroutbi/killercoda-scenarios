777098426463db4b1dfbda95cfc36937bee6034e
Sergio Arroutbi <sarroutb@redhat.com>
2022-03-18 11:39:36 +0100

    Fix messages in start and tang operator installation
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tang-operator/install-tang-operator.md
+++ tang-operator/install-tang-operator.md
@@ -1,1 +1,2 @@
-Install operator-sdk via `operator-sdk run bundle --verbose --timeout 10m quay.io/sec-eng-special/tang-operator-bundle:latest`{{execute}}
+Install tang operator by using next command:
+`operator-sdk run bundle --verbose --timeout 10m quay.io/sec-eng-special/tang-operator-bundle:latest`{{execute}}
--- tang-operator/start.sh
+++ tang-operator/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sleep 3
+sleep 5
 launch-k8s.sh
 install-operator-sdk.sh
+echo "==================================================================="
+echo "          K8S cluster is ready, operator-sdk is installed          "
+echo "                  Please, continue with next steps                 "
+echo "==================================================================="
