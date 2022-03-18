43ba18df799f4ba17277269c344ae934054b8646
Sergio Arroutbi <sarroutb@redhat.com>
2022-03-18 11:06:43 +0100

    Fix launch-k8s.sh execution
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tang-operator/launch-k8s.sh
+++ tang-operator/launch-k8s.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo Waiting for Kubernetes to start...
-  while [ ! -f /root/.kube/config ]
-  do
-    sleep 1
-  done
+while [ ! -f /root/.kube/config ]
+do
+  sleep 1
+done
 echo Kubernetes started
-if [ -f /root/.kube/start ]; then
+if [ -f /root/.kube/start ];
+then
   /root/.kube/start
 fi
